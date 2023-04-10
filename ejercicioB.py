9f8f2eb0e3bc60a8dcde85a4e3fb4426efcdf893
jone <jvegadel@myuax.com>
2023-04-10 17:16:33 +0200

    b

--- ejercicioB.py
+++ ejercicioB.py
@@ -18,3 +18,22 @@ del(yang)
 print("?")
 >>> ?
 Yang destruido"""
+
+class Yin: pass
+class Yang:
+    def __del__(self):
+        print("Yang destruido")
+
+yin = Yin()
+yang = Yang()
+yin.yang = yang
+print(yang)
+
+print(yang is yin.yang)
+
+del(yang)
+print("?")
+
+
+#Respuesta:
+"""El mensaje Yang destruido se muestra después del signo de interrogación porque cuando se llama a del(yang),se destruye el objeto yang y se llama automaticamente al metodo __del__ de la clase Yang, que es el que imprime el mensaje Yang destruido. Para que aparezca antes, hay que llamar al metodo __del__ de la clase Yang antes de llamar a del(yang)."""
